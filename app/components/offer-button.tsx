b9ee77f927474aa10e37ebb0ad36e7652e832c94
Sylvester Alderson <51517881+SylvesterFox@users.noreply.github.com>
2025-06-02 19:48:26 +0300

    main

---
+++ app/components/offer-button.tsx
@@ -0,0 +1,5 @@
+export default function OfferButton() {
+    return (
+        <a href="" className="text-[40px] font-bold bg-[var(--color-element-background)] px-[80px] py-[20px] rounded-[40px] drop-shadow-lg transition duration-500 hover:bg-white">Стать курьером</a>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/header";
+import Main from "./components/main";
 
 
 export default function Home() {
   return (
      <div className="h-[150vh]">
         <Header />
+        <Main />
      </div>
     
   );
